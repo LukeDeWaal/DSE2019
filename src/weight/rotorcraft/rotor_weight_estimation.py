393a5a4772aed2dfd088751b9a223cb91bb6ce7f
mvanlobensels <mavanlobensels@gmail.com>
2019-05-10 14:49:19 +0200

    rotor estimations

--- src/weight/rotorcraft/rotor_weight_estimation.py
+++ src/weight/rotorcraft/rotor_weight_estimation.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class SingleRotorSizingEstimation:
     
-    def __init__(self, MTOW):
+    def __init__(self, MTOW, n_blades):
         self.MTOW = MTOW
+        self.n_blades = n_blades
         self.D_L = self.disk_loading()
         self.d = self.rotor_diameter()
 
@@ -29,8 +30,12 @@ class SingleRotorSizingEstimation:
         """
         Vmax = (9.133*self.MTOW**(0.380)/self.d)**1/0.515
         return Vmax
+    
+    def chord_length(self):
+        c = 0.0108*self.MTOW**0.539/self.n_blades**0.714
+        return c
    
     
 if __name__ == '__main__':
-    s = SingleRotorSizingEstimation(4000)
-    print(s.maximum_speed()*3.6)
+    s = SingleRotorSizingEstimation(4000, 4)
+    print(s.chord_length())
