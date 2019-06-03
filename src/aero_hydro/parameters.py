43aa6d36764caa24944b37385ede98d65cea50f6
mvanlobensels <mavanlobensels@gmail.com>
2019-06-03 11:40:36 +0200

    control surface plot

--- src/aero_hydro/parameters.py
+++ src/aero_hydro/parameters.py
@@ -3,7 +3,7 @@ import numpy as np
 # General parameters
 MTOW = 9000 # kg
 aircraft_length = 9 # m
-cg_position = aircraft_length * 0.4
+cg_position = aircraft_length * 0.3
 rho = 1.225 # kg/m^3
 
 # Wing parameters
@@ -24,15 +24,22 @@ max_tail_span = 4 # m
 tail_position = 8.75 # m
 tail_force = wing_moment/(tail_position - cg_position) # N
 Cl_h = -0.5
-#S_h = -tail_force/(Cl_h*0.5*rho*V_cruise**2)
-#tail_chord = S_h/max_tail_span
 L_ht = tail_position - wing_position
 horizontal_tail_volume_coefficient = 0.5
 horizontal_tail_area = horizontal_tail_volume_coefficient * (c*S)/L_ht
-horizontal_tail_chord = horizontal_tail_area/max_tail_span
+horizontal_tail_chord = horizontal_tail_area/max_tail_span * 0.9
 
 # Vertical tail parameters
-vertital_tail_volume_coefficient = 0.04
+vertital_tail_volume_coefficient = 0.05
+vertical_tail_area = vertital_tail_volume_coefficient*b*S/L_ht * 0.9
+
+# Control surface parameters
+aileron_length = 0.4 * b / 2
+aileron_chord = 0.2 * c
+elevon_length = 0.8 * max_tail_span
+elevon_chord = 0.375 * horizontal_tail_chord
+
+
 
 
 
