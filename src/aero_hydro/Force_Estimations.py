e99ad1ce2723dc9ab30ee16cad4bea05ba785318
Patty <patricia.apostol@gmail.com>
2019-06-03 10:03:48 +0200

    Force  Estimations file

---
+++ src/aero_hydro/Force_Estimations.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 09:43:00 2019
+
+@author: patri
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from wing_surface_locations import *
+
