c6e25732484b957159379bdd884719347411615a
Patty <patricia.apostol@gmail.com>
2019-06-03 10:57:01 +0200

    Updated parameters

--- src/aero_hydro/Force_Estimations.py
+++ src/aero_hydro/Force_Estimations.py
@@ -9,4 +9,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from wing_surface_locations import *
+from parameters import *
+
+######PARASITE DRAG#####
+
+def parasite_drag():
+    ###This function outputs the parasite drag of the various aircraft components###
+    
+    #Wing pararsite drag
+    
+    S_wing_wet = 2*1.07*S_wing
+    
+    
+    
+    
+    
+
+    
+    
+
+
+
 
