8d17ee45cd35c2c09aed167b36a61ca15c03d1ce
Patty <patricia.apostol@gmail.com>
2019-06-03 16:59:54 +0200

    Version 1 for parasitic drag estimations

--- src/aero_hydro/Force_Estimations.py
+++ src/aero_hydro/Force_Estimations.py
@@ -30,17 +30,21 @@ def skinfriction_drag(S_ref,R,M, lt_ratio,t_c,x_c_max,sweep_angle_max,k,l,compon
         
     
     #Wetted areas
-    
     if component == 'wing':
-        S_wet = 2*1.07*S_ref
-    elif component == 'horizontal tail' or component == "vertical tail":
-        S_wet= 2*1.05*S_ref
+        S_wet = 2*1.07*S_wing
+    elif component == 'horizontal tail':
+        S_wet= 2*1.05*horizontal_tail_area
+    elif component == 'vertical tail':
+        S_wet = 2*1.05*vertical_tail_area
     elif component == 'fuselage':
-        S_wet = (np.pi*fus_B/4)*(1/(3*fus_len_1**2)*((4*fus_len_1**2+fus_B**2/4)**1.5-fus_B**3/8)-fus_B+4*fus_len_2+2*np.sqrt(fus_len_3**2+fus_B**2/4))
+        S_wet = (np.pi*(fus_B*2)/4)*(1/(3*fus_len_1**2)*((4*fus_len_1**2+(fus_B*2)**2/4)**1.5-(fus_B*2)**3/8)-(fus_B*2)+4*fus_len_2+2*np.sqrt(fus_len_3**2+(fus_B*2)**2/4))
+    elif component == 'nacelle':
+        S_wet = (np.pi*(cowling_diameter**2)/4)*engine_length
         
     
     #The next parameter is dependent on the type of material used in the design
-    R_cutoff = 38.21*(l/k)**1.053
+    R_cutoff = 38.21*((l/k)**1.053)
+
     
     #The lower of either the actual Reynolds or cutoff must be chosen, the cutoff gives an idea of where
     #transition occurs
@@ -66,27 +70,68 @@ def skinfriction_drag(S_ref,R,M, lt_ratio,t_c,x_c_max,sweep_angle_max,k,l,compon
     #Correction for Fuselage Skin Friction
     if component == 'fuselage':
         
-        f=fus_l/(np.sqrt((4/np.pi)*fus_front_area))
+        f=l/(np.sqrt((4/np.pi)*fus_front_area))
         
-        FF=1+60/f**3+f/400
+        FF=1+(60/f**3)+f/400
         
         #Scaling due to flying boat hull
         FF=FF*1.5
-        
     
+        S_wet = 114
+    if component == 'nacelle':
+        f = l/(np.sqrt((4/np.pi)*(np.pi*(cowling_diameter**2)/4)))
+        
+        FF= 1 + (0.35/f)
+        
+        
+        
     Cd0_sf = FF*S_wet*Cf_final/S_ref
         
     return Cd0_sf
+
+def miscellaneous_drag():
+    ###This function outputs other parasitic drag sources
+    
+    #fuselage upsweep:
+    D_q=3.83*fus_upsweep*np.pi/180**2.5*fus_front_area
+    
+    S_ref=S_wing
+    
+    Cd0_fus_upsweep = D_q/S_ref
+    
+    
+    #Landing Gear:
+    #For the moment for landing gear statistical data is used as it hasn't yet been sized
+    
+    Cd0_land=(2*0.25/S_ref)*1.2
+    
+    Cd0_misc_tot=Cd0_land+Cd0_fus_upsweep
+    
+    return Cd0_misc_tot
 
 
 
 ####MAIN####
     
-Parasite_Wing = (skinfriction_drag(R_cruise_wing,M_cruise,0.2,0.15,0.4,0,k_paint,c,'wing'))
+Sf_wing = (skinfriction_drag(S_wing, R_cruise_wing,M_cruise,0.2,0.15,0.5,0,k_comp,c,'wing'))
 
-Parasitte_ht = (skinfriction_drag(R_cruise_ht,M_cruise,0.2,0.15,0.4,0,k_paint,c,'wing'))
+Sf_ht = (skinfriction_drag(S_wing, R_cruise_ht, M_cruise,0.2,0.12,0.3,0,k_comp,horizontal_tail_chord,'horizontal tail'))*1.05
 
-#Parasite_ht = skinfriction_drag(R_cruise_tail,M_cruise,0.2,0.15,0.4,0,k_paint,fuselage = False)
+Sf_vt = (skinfriction_drag(S_wing,R_cruise_ht,M_cruise,0.2,0.15,0.4,0,k_comp,horizontal_tail_chord,'vertical tail'))*1.05
+
+Sf_fus = (skinfriction_drag(S_wing,R_cruise_fus,M_cruise,0.25,0.7,0.2,0,k_comp,fus_l,'fuselage'))
+
+Sf_nacelle = (skinfriction_drag(S_wing,R_cruise_eng,M_cruise,0.2,0.7,0.2,0,k_comp,engine_length,'nacelle'))*1.5
+
+Sf_total = Sf_wing +Sf_ht + Sf_vt+ Sf_fus + Sf_nacelle
+
+Total_Parasitic = Sf_total+miscellaneous_drag()
+
+print("The Total Skin Friction Drag is", Sf_total)
+
+print ("The Total Parasitic Drag is", Total_Parasitic)
+
+
 
 
 
--- src/aero_hydro/parameters.py
+++ src/aero_hydro/parameters.py
@@ -14,7 +14,7 @@ AR = 7.5
 S_wing = 50 # m^2
 b = np.sqrt(AR*S_wing) # m
 c = S_wing/b # m
-V_cruise = 103 # m/s
+V_cruise = 97 # m/s
 V_loit = 44 # m/s
 M_cruise=V_cruise/np.sqrt(1.4*287*273)
 Cl_cruise = 0.45046
@@ -36,18 +36,23 @@ horizontal_tail_area = horizontal_tail_volume_coefficient * (c*S_wing)/L_ht
 horizontal_tail_chord = horizontal_tail_area/max_tail_span * 0.9
 
 # Vertical tail parameters
-vertital_tail_volume_coefficient = 0.04
-vertital_tail_volume_coefficient = 0.05
-vertical_tail_area = vertital_tail_volume_coefficient*b*S_wing/L_ht * 0.9
+vertical_tail_volume_coefficient = 0.04
+vertical_tail_volume_coefficient = 0.05
+vertical_tail_area = vertical_tail_volume_coefficient*b*S_wing/L_ht * 0.9
 
 #Fuselage parameters
 fus_l=9.0 #m
 fus_A=0.75
 fus_B=1.25
 fus_front_area=fus_A*fus_B*np.pi
-fus_len_1=0
-fus_len_2=0
-fus_len_3=0
+fus_len_1=2.25
+fus_len_2=1.53
+fus_len_3=5.22
+fus_upsweep = 15 #deg
+
+#Engine parameters
+cowling_diameter = 1.4*0.4
+engine_length=6.0
 
 # Control surface parameters
 aileron_length = 0.4 * b / 2
@@ -61,8 +66,13 @@ hull_width = 1.5 # m
 #Aerodynamics Parameters
 R_cruise_wing=rho_cruise*V_cruise*c/miu_cruise
 R_cruise_ht=rho_cruise*V_cruise*horizontal_tail_chord/miu_cruise
+R_cruise_fus=rho_cruise*V_cruise*fus_l/miu_cruise
+R_cruise_eng=rho_cruise*V_cruise*engine_length/miu_cruise
+
+
 mach = 0.286
 beta = np.sqrt(1-mach**2)
 Cl_alpha = 6.25
 airfoil_efficiency = Cl_alpha/(2*np.pi/beta) 
 CL_alpha = 2*np.pi*AR/(2 + np.sqrt(4 + (AR*beta/airfoil_efficiency)**2 * (1 + np.tan(sweep)**2))) * (S_wing - c*hull_width)/S_wing * 1.07*(1+hull_width/b)**2
+
